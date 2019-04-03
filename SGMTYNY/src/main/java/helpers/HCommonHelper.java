ef0987f075b7c6d1829c68667f13c60f3b70d0c8
diegomejia <diegomejia@DT39538.mx.deloitte.com>
2019-04-03 12:57:18 -0600

    helpers random

--- SGMTYNY/src/main/java/helpers/HCommonHelper.java
+++ SGMTYNY/src/main/java/helpers/HCommonHelper.java
@@ -1,9 +1,22 @@
 package helpers;
 
+import java.util.Random;
+
 public class HCommonHelper {
-	
+	private static Random random;
 	public static int getLenghtUrl(String url) {
 		return url.length();
+	}
+	public static int getRandomNumberInRange(int limit) {
+		random = new Random();
+		return random.nextInt(limit);
+	}
+	public static String getRandomEmail() {
+		random = new Random();
+		String[] emailDomain = {"hotmail","gmail","yahoo"};
+		String[] names = {"Iniesta","Ronaldinho","Ronaldo","Zlatan","Griezmann","Neymar","Beckham","Mbappe"};
+		String[] specialCharacter = {"_",".",""};
+		return names[random.nextInt(names.length)]+specialCharacter[random.nextInt(specialCharacter.length)]+random.nextInt(10)+"@"+emailDomain[random.nextInt(emailDomain.length)]+".com";
 	}
 	public String getRandomName() {		
 		return null;
