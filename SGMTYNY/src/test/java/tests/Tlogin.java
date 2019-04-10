9b3588f84b3bf5e9d7eb11155386cc3bc3c292f7
AbrahamGalindo <a_galindob@hotmail.com>
2019-04-10 12:43:43 -0500

    Creacion de nuevas clases

--- SGMTYNY/src/test/java/tests/Tlogin.java
+++ SGMTYNY/src/test/java/tests/Tlogin.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import org.testng.annotations.Test;
 
+import helpers.HReporte;
 import pages.PageInicio;
 import pages.PageLogin;
 
@@ -19,16 +20,27 @@ public class Tlogin extends BaseTest{
 	
 	@Test
 	public static void stepsLogin() throws IOException {
+		try {
 		
 		
 		strPaso = "Creando el page login";
 		PageLogin login = new PageLogin(driver);
-	//	PageInicio inicio = new PageInicio(driver);
+		
+		
+		strPaso = "Llamando el reporte";
+		HReporte reporte = new HReporte();
+		
+		
 		strPaso = "Invocando metodos a login";
 		login.launchHome();
 		login.inputUsuario();	
 		login.inputContrasenia();
 		login.clickEntrar();
+		}
+		
+		catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		    }
 		//inicio.clickReferidos();
 		
 	}	
